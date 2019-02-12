f1912ae2562bb0616389bcb0081ba8e2df2dd236
scomari <simone.comari2@unibo.it>
2019-02-12 17:51:03 +0100

    included pid reset function + fixed bug in pid init

--- src/ctrl/controller_singledrive_naive.cpp
+++ src/ctrl/controller_singledrive_naive.cpp
@@ -8,8 +8,7 @@ constexpr double ControllerSingleDriveNaive::kMaxCtrlOutput_;
 
 ControllerSingleDriveNaive::ControllerSingleDriveNaive(const uint32_t period_nsec)
   : ControllerBase(), period_sec_(period_nsec * 0.000000001),
-    torque_pid_(period_sec_, ControllerSingleDriveNaive::Kp_, Ki_, Kd_, Tf_,
-                kMaxCtrlOutput_, -kMaxCtrlOutput_)
+    torque_pid_(period_sec_, Kp_, Kd_, Ki_, Tf_, kMaxCtrlOutput_, -kMaxCtrlOutput_)
 {
   Clear();
   abs_delta_torque_ = period_sec_ * kAbsDeltaTorquePerSec_; // delta per cycle
@@ -18,7 +17,7 @@ ControllerSingleDriveNaive::ControllerSingleDriveNaive(const uint32_t period_nse
 ControllerSingleDriveNaive::ControllerSingleDriveNaive(const id_t motor_id,
                                                        const uint32_t period_nsec)
   : ControllerBase(vect<id_t>(1, motor_id)), period_sec_(period_nsec * 0.000000001),
-    torque_pid_(period_sec_, Kp_, Ki_, Kd_, Tf_, kMaxCtrlOutput_, -kMaxCtrlOutput_)
+    torque_pid_(period_sec_, Kp_, Kd_, Ki_, Tf_, kMaxCtrlOutput_, -kMaxCtrlOutput_)
 {
   Clear();
   abs_delta_torque_ = period_sec_ * kAbsDeltaTorquePerSec_; // delta per cycle
@@ -122,12 +121,12 @@ ControllerSingleDriveNaive::CalcMotorTorque(const vect<ActuatorStatus>& actuator
     double current_motor_torque = static_cast<double>(actuator_status.motor_torque);
     motor_torque =
       current_motor_torque + torque_pid_.Calculate(torque_target_, current_motor_torque);
-    printf("%f/%f --> %d/%d\n", current_motor_torque, torque_target_,
-           static_cast<int16_t>(round(motor_torque)), torque_target_true_);
+//    printf("%f/%f --> %d/%d\n", current_motor_torque, torque_target_,
+//           static_cast<int16_t>(round(motor_torque)), torque_target_true_);
     break;
   }
-  //  return static_cast<int16_t>(round(motor_torque));
-  return torque_target_true_;
+  return static_cast<int16_t>(round(motor_torque));
+  //  return torque_target_true_;
 }
 
 vect<ControlAction>
@@ -192,6 +191,9 @@ ControllerSingleDriveNaive::CalcCtrlActions(const grabcdpr::Vars&,
 void ControllerSingleDriveNaive::Clear()
 {
   target_flags_.ClearAll();
+
+  torque_pid_.Reset();
+  printf("pid reset\n");
 
   change_length_target_ = false;
   change_torque_target_ = false;
